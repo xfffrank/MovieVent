ecb0593eaff397c83a3643d45ec45778e6f528f0
Vent <546706835@qq.com>
2018-04-15 16:53:31 +0800

    before deployment

--- MovieVent/settings.py
+++ MovieVent/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '46d_(35g25pa6youv9ss6g^7u01-r%byq=#=omkv_0ai*6807+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.movie.croissant.top']
 
 
 # Application definition
